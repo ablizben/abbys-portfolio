764937890d13ed78ca27ff3f58e11b9fc435dcef
Abby Benjamin <ablizben@gmail.com>
2021-02-03 10:49:14 -0500

    fixing class name and js

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-$(document).ready(function () {
-  //scroll to about me section
-  $("#move-to-about-me").click(function () {
-    $("body,html").animate({
-        scrollTop: $(".about-me-container").offset().top
-      },
-      100 //speed
-    );
-  });
-  //scroll to projects section
-  $("#move-to-projects").click(function () {
-    $("body,html").animate({
-        scrollTop: $(".project-container").offset().top
-      },
-      100 //speed
-    );
-  });
-});
-//scroll to contact section
-$("#move-to-contact").click(function () {
+//scroll to about me section
+$("#move-to-about-me").click(function () {
   $("body,html").animate({
-      scrollTop: $(".contact-container").offset().top
+      scrollTop: $("#about-me-container").offset().top
     },
     100 //speed
   );
+});
+//scroll to projects section
+$("#move-to-projects").click(function () {
+  $("body,html").animate({
+      scrollTop: $("#project-container").offset().top
+    },
+    100 //speed
+  );
+});
 
+//scroll to contact section
+$("#move-to-contact").click(function () {
+  $("body,html").animate({
+      scrollTop: $("#contact-container").offset().top
+    },
+    100 //speed
+  );
 });
