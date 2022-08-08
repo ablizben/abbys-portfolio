52f9f4fba220ec2b03892eac6380a4a31e5a8042
Abby Benjamin <ablizben@gmail.com>
2022-08-07 23:02:16 -0400

    removing projects section and editing about me to reflect life

--- main.js
+++ main.js
@@ -19,14 +19,6 @@ $("#move-to-about-me").click(function () {
     100 //speed
   );
 });
-//scroll to projects section
-$("#move-to-projects").click(function () {
-  $("body,html").animate({
-      scrollTop: $("#project-container").offset().top
-    },
-    100 //speed
-  );
-});
 
 //scroll to contact section
 $("#move-to-contact").click(function () {
